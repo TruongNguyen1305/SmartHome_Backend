b7ace7e4743b8eb8dace2e86f5f5c223f4cad8e4
Nam Nguyễn <nguyenphinam2k2@gmail.com>
2023-03-22 21:04:23 +0700

    edit model

--- models/User.js
+++ models/User.js
@@ -11,7 +11,6 @@ const UserSchema = new mongoose.Schema({
     homeID: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: 'Home',
     },
 }, {
     timestamps: true
